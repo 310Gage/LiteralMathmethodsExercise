ba0eaa04c53e528a07958d2b258a39f4eed26a0d
Gage Hall <Gageh310@gmail.com>
2022-09-29 11:40:00 -0400

    completed Literal Math Methods exercise

---
+++ script.js
@@ -0,0 +1,27 @@
+let warmHugs = "hi, I'm olaf and I like warm hugs";
+console.log(warmHugs.toUpperCase());
+
+warmHugs = warmHugs.replace('like', 'love');
+console.log(warmHugs);
+
+let beenImpaled = 'Oh, look at that. I\'ve been impaled.';
+console.log(beenImpaled.slice(-18));
+
+let dotDotDot = '...';
+let SkullBones = `i don't have a skull${dotDotDot}or bones`;
+console.log(SkullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log(randomNumber);
+
+console.log(' Let It Go!'.repeat(2));
+
+let reindeers = 'Reindeers are better than people.';
+console.log(reindeers.replaceAll(" ", "_"));
+
+console.log(Math.sqrt(2));
+
+let newRandomNumber = Math.floor(Math.random()*(17)+7);
+console.log(newRandomNumber);
